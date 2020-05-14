143cf8418e547747fe4e8a89e324c0b770271d47
vinzolivera <37941922+vinzolivera@users.noreply.github.com>
2020-05-14 08:40:59 -0300

    loops

--- aulas/Curso1_aula_loops.py
+++ aulas/Curso1_aula_loops.py
@@ -12,6 +12,7 @@ lista = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 numeros = range(1,10)
 
 #Exemplo 1
+print(' ') 
 print("Exemplo for em string")
 for letra in nome:
     print(letra.upper(), end = ' ')
@@ -19,24 +20,13 @@ for letra in nome:
 print(' ')
 
 #Exemplo 2
+print(' ') 
 print("Exemplo for em lista")
 for num in lista:
     print(pow(num,2), end = ' ')
    
 print(' ')   
  
-#Exemplo 3 - no range o ultimo valor não é incluído
-print("Exemplo for em range")   
-for x in numeros:
-    print(x, end = ' ')
-    
-print(' ') 
-
-#Exemplo 4 - gera uma tupla com indice e valor do iteravel
-print("Exemplo for em enumerate")
-for i in enumerate(nome[0:8]):
-    print(i,end = ' ')  
-print(" ")    
 """
 ranges são geradores de sequencia numerica de maneira
 especificada (ordenada). Por padrão a sequencia é
@@ -58,6 +48,21 @@ range(valor_inicial,valor_de_parada, passo)
     passo especificado até o valor de parada -1 
 
 """
+ 
+#Exemplo 3
+print(' ')  
+print("Exemplo for em range")   
+for x in numeros:
+    print(x, end = ' ')
+    
+print(' ') 
+
+#Exemplo 4 - gera uma tupla com indice e valor do iteravel
+print(' ') 
+print("Exemplo for em enumerate")
+for i in enumerate(nome[0:8]):
+    print(i,end = ' ')  
+print(" ")    
 
 """
 Loops while são utilizados para iterar sobre sequencias
@@ -75,6 +80,31 @@ while numero < 10:
  
 print(" ") 
  
+#Exemplo 6 
+print(' ') 
+print("Exemplo while - iteração com strings") 
+
+frase = "Um dia frio, um bom lugar para ler um livro"
+tamanho = len(frase)
+i = 0
+letra = ''
+cont = 0
+
+while i < tamanho:
+    qtd_letra = frase.count(frase[i])
+    
+    if cont < qtd_letra and frase[i].strip():
+        letra = frase[i]
+        cont = qtd_letra
+    
+    
+    i+=1
+print(frase)
+print(f'A {letra} aparece {cont} vezes na frase')     
+
+
+
+
 """
 O comando break é utilizado para sair de um loop
 de maneira "forçada".
@@ -91,7 +121,8 @@ seja por esgotamento de iteravel em um loop for ou por
 uma condição falsa em loop while
  
 """
-#Exemplo 6 - comando break
+#Exemplo 7 - comando break
+print(' ') 
 print("Exemplo break") 
 for i in range(1,10):
     if i == 7:
@@ -100,7 +131,8 @@ for i in range(1,10):
          print(i, end = ' ')         
 print("Saiu do loop")
 
-#Exemplo 7 - comando continue
+#Exemplo 8 - comando continue
+print(' ') 
 print("Exemplo continue") 
 
 for i in range(1,10):
@@ -109,7 +141,8 @@ for i in range(1,10):
     print(i, end = ' ')
 print(" ")
          
-#Exemplo 8 - comando pass
+#Exemplo 9 - comando pass
+print(' ') 
 print("Exemplo pass") 
 for i in range(1,10):
     if i == 3:
@@ -118,9 +151,10 @@ for i in range(1,10):
     print(i, end = ' ')     
 print(" ")        
 
-#Exemplo 9 - comando else em loops
-soma=0
+#Exemplo 10 - comando else em loops
+print(' ') 
 print("Exemplo else em loops") 
+soma=0
 for n in range(1, 11):
     soma+=n
 else:
